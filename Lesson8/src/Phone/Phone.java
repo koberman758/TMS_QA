f3bfb9fc2da2b9d3774bdba77cae8c439fbf0d88
koberman758 <koberman758@gmail.com>
2023-01-11 09:43:23 +0300

    homework task1

---
+++ Lesson8/src/Phone/Phone.java
@@ -0,0 +1,65 @@
+package Phone;
+
+public class Phone {
+    private int number;
+    private String model;
+    private int weight;
+
+
+    public void receiveCall(String name) {
+        System.out.println("Звонит " + name);
+    }
+
+    public void receiveCall(String name, int number) {
+        System.out.println(name + " " + number);
+    }
+
+
+    public Phone() {
+    }
+
+    public Phone(int number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone(int number, String model, int weight) {
+        this(number, model);
+        this.weight = weight;
+    }
+
+    public int[] sendMessage(int... var) {
+        return var;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String toString() {
+        return number + " " + model + " " + weight;
+    }
+
+
+}
+
