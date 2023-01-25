9ae2c867bf6cde70496ff9172b0b618815d2d56c
koberman758 <koberman758@gmail.com>
2023-01-25 14:38:19 +0300

    task3

--- Java/src/HW2/Main.java
+++ Java/src/HW2/Main.java
@@ -1,7 +1,6 @@
 package HW2;
 
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,27 +8,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-            Student student1 = new Student("Alex", "G1", 10);
-            Student student2 = new Student("Tom", "G2", 6);
-            Student student3 = new Student("Ben", "G3", 4);
-            Student student4 = new Student("Jan", "G4", 7);
-            Student student5 = new Student("Bob", "G5", 3);
+        Student student1 = new Student("Alex", "G1", 10);
+        Student student2 = new Student("Tom", "G2", 6);
+        Student student3 = new Student("Ben", "G3", 4);
+        Student student4 = new Student("Jan", "G4", 7);
+        Student student5 = new Student("Bob", "G5", 3);
 
-            List<Student> students = List.of(student1, student2, student3, student4, student5);
+        List<Student> students = List.of(student1, student2, student3, student4, student5);
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("введите имя студента");
-            String name = scanner.next();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите имя студента");
+        String name = scanner.next();
 
-            for (Student student: students)
-            {
-                if(student.getName() != null && student.getName().equals(name))
-                {
-                    System.out.println(student.getGroup() + " " + student.getMark());
-                    return;
-                }
-
+        for (Student student : students) {
+            if (student.getName() != null && student.getName().equals(name)) {
+                System.out.println(student.getGroup() + " " + student.getMark());
+                return;
             }
 
         }
+
+    }
 }
